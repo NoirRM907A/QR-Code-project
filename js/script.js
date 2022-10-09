dd6a42c413d879a6e4fb7a4995eb12fcc4bb2421
NoirRM907A <53484018+NoirRM907A@users.noreply.github.com>
2022-10-08 19:02:57 -0600

    script.js updated

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ generateBtn.addEventListener("click",()=> {
     let qrValue = qrInput.value;
     if(!qrValue) return;
         generateBtn.innerText="Generating QR Code...";
-        qrImg.scr = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+        qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
         qrImg.addEventListener("load", ()=>{
             wrapper.classList.add("active");
             generateBtn.innerText="Generate QR Code";
